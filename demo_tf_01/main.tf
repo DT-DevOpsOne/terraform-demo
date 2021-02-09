d1438249798b04ca4c17e9134d5cc7ffe55f8f2f
Kevin Bayliss <7736942+kevinbayliss@users.noreply.github.com>
2021-02-09 10:51:23 +0000

    Add files via upload

---
+++ demo_tf_01/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  access_key = "Enter here"
+  secret_key = "Enter here"
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "demo" {
+  ami = "ami-0bb3fad3c0286ebd5"
+  instance_type = "t2.micro"
+  key_name = "TU_Dublin"
+  tags = {
+	Name = "Demo"
+  }
+}
