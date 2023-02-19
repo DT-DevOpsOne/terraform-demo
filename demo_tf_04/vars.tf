8bb04ec2cd3d46d80da6b5bc42eb0ff7c53b6502
DT-DevOpsOne <105010838+DT-DevOpsOne@users.noreply.github.com>
2023-02-19 20:48:36 +0000

    Update vars.tf

--- demo_tf_04/vars.tf
+++ demo_tf_04/vars.tf
@@ -1,16 +1,17 @@
 variable "ACCESS_KEY" {}
 variable "SECRET_KEY" {}
+variable "TOKEN" {}
 
 variable "AMI" {
   type = map(string)
   default = {
-    eu-west-1 = "ami-0bb3fad3c0286ebd5"
-    eu-west-2 = "ami-0a669382ea0feb73a"
+    us-east-1 = "ami-0557a15b87f6559cf"
+    eu-east-2 = "ami-0a669382ea0feb73a"
   }
 }
 
 variable "REGION" {
-  default = "eu-west-1"
+  default = "us-east-1"
 }
 
 variable "INSTANCE_TYPE" {
