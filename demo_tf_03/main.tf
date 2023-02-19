35448b09a729784c4bb5a2ea298777526fbacde0
DT-DevOpsOne <105010838+DT-DevOpsOne@users.noreply.github.com>
2023-02-19 20:14:58 +0000

    Update main.tf

--- demo_tf_03/main.tf
+++ demo_tf_03/main.tf
@@ -1,11 +1,12 @@
 provider "aws" {
   access_key = "Enter here"
   secret_key = "Enter here"
-  region = "eu-west-1"
+  token = "Enter here"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "demo" {
-  ami = "ami-0bb3fad3c0286ebd5"
+  ami = "ami-0557a15b87f6559cf"
   instance_type = "t2.micro"
   key_name = "TU_Dublin"
   tags = {
